034aaa373a31cd2f19cf4a96d475afd5c73bab72
Ninja Reny <chrishanth.niranjan@gmail.com>
2025-06-02 13:25:11 +0530

    migration error fix

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schemas/*',
+  schema: './src/db/schemas/**',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
