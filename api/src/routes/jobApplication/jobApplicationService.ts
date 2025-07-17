5d35610b29b8e9d03a177ca33dcc455e44bd475f
Ninja Reny <chrishanth.niranjan@gmail.com>
2025-07-17 10:26:42 +0530

    change in complete job notification

--- api/src/routes/jobApplication/jobApplicationService.ts
+++ api/src/routes/jobApplication/jobApplicationService.ts
@@ -735,7 +735,7 @@ export class JobApplicationService {
       // Notify healthcare worker
       await NotificationService.createFromTemplate(
         'JOB_COMPLETED',
-        application.healthcareUserId,
+        userId,
         {
           jobTitle: application.jobPost.title,
           jobPostId: application.jobPostId,
@@ -743,7 +743,7 @@ export class JobApplicationService {
         {
           jobPostId: application.jobPostId,
           jobApplicationId: application.id,
-          relatedUserId: userId,
+          relatedUserId: application.healthcareUserId,
           sendEmail: true,
         }
       );
