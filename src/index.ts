b5ced35b4143f7d3022a6f666d9c66893ad5d8fa
Rohit Nishad <r.n.rohitnishad@gmail.com>
2025-08-28 00:48:13 +0530

    cors fix

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ global.socketManager = socketManager;
 // Enable CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://test.medcarr.co.uk"],
     credentials: true,
   }),
 );
